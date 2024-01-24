e1bce634a841d72bc177937d5c129f84d630430e
Zion1TECH <157618700+Zion1TECH@users.noreply.github.com>
2024-01-24 20:19:32 +0300

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const rl = require('readline-sync');
 const colors = require('colors');
 const title = require('./modules/title.js');
-const TURK HACK TEAM = require('./modules/sms.js');
+const TeknoEvreniSmsBomb = require('./modules/sms.js');
 
-title('ZioN TECH');
+title('Hosgeldiniz');
 
-let telefon = rl.question('Saldırılacak Numara ? : ' .white);
+let telefon = rl.question('Saldırılacak Numara ? : ' .green);
 if (telefon.length != 10) {
     console.log('Düzgün Gir AMK Şu Numarayı'.white);
     process.exit(1);
 }
 title('Numara: ' + telefon);
 
-let miktar = rl.question("( 01-800 ) Adet Gir : ".white);
+let miktar = rl.question("( 01-800 ) Adet Gir : ".green);
 if(isNaN(miktar)) return console.log('Lutfen Bir Rakam Giriniz'.red) && process.exit(1);
 if (miktar.length == 0) {
     console.log('Miktar Giriniz'.red);
