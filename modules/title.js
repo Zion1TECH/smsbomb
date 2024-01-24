4f87d3140b138ff91f772a0e09e170fd3d47517a
Zion1TECH <157618700+Zion1TECH@users.noreply.github.com>
2024-01-24 21:06:37 +0300

    title.js

--- modules/title.js
+++ modules/title.js
@@ -2,11 +2,11 @@ function console_title(x) {
 
     if (process.platform == 'win32') {
 
-        process.title = x + " / TeknoEvreniSmsBomb v1.0 - github.com/teknodroid";
+        process.title = x + " / TurkHackTeam v1.0 - github.com/Zion1TECH";
 
     } else {
 
-        process.stdout.write('\x1b]2;' + x + " / TeknoEvreniSmsBomb v1.0 - github.com/teknodroid" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / TurkHackTeam v1.0 - github.com/Zion1TECH" + '\x1b\x5c');
 
     }
 
